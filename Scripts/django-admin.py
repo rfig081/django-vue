d90db7a9e75c1f9daea5f8dfb6538d1e97513103
Reynold Figueroa <reynold0801@outlook.com>
2019-02-13 17:18:34 -0400

    Initial commit

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\reynold\documents\github\django-vue\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ src/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <MyHeader></MyHeader>
+    <h1> Some content... </h1>
+    <MyFooter></MyFooter>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+
+export default {
+    components: {
+        'MyHeader': Header,
+        'MyFooter': Footer,
+    }
+}
+</script>
+
+<style scoped lang="sass">
+html, body
+  margin: 0
+  min-height: 100vh
+</style>
